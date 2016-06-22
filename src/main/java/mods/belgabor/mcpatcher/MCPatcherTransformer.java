ac05ef39d6f70700f2291a28738a953d1d074db5
Belgabor <belgabor@gmx.de>
2016-06-22 06:28:20 +0200

    More fixes * Should fix cout970/Magneticraft-API-and-Issues#77 / cout970/Magneticraft-API-and-Issues#82 * Maybe fixes cout970/Magneticraft-API-and-Issues#92

--- src/main/java/mods/belgabor/mcpatcher/MCPatcherTransformer.java
+++ src/main/java/mods/belgabor/mcpatcher/MCPatcherTransformer.java
@@ -2,10 +2,7 @@ package mods.belgabor.mcpatcher;
 
 import net.minecraft.launchwrapper.IClassTransformer;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.*;
 import squeek.asmhelper.com.belgabor.mcpatcher.ASMHelper;
 import squeek.asmhelper.com.belgabor.mcpatcher.ObfHelper;
 
@@ -19,7 +16,9 @@ import java.util.Arrays;
 public class MCPatcherTransformer implements IClassTransformer {
     
     private static final String[] classesBeingTransformed = {
-            "com.cout970.magneticraft.util.InventoryComponent"
+            "com.cout970.magneticraft.util.InventoryComponent",
+            "com.cout970.magneticraft.tileentity.shelf.TileShelf",
+            "com.cout970.magneticraft.tileentity.shelf.TileShelfFiller"
     };
     
     @Override
@@ -30,8 +29,6 @@ public class MCPatcherTransformer implements IClassTransformer {
 
     private byte[] transform(int index, byte[] classBeingTransformed) {
         System.out.println("Transforming: " + classesBeingTransformed[index]);
-        if (ObfHelper.isObfuscated())
-            System.out.println("Obfuscated Environment");
         try
         {
             ClassNode classNode = ASMHelper.readClassFromBytes(classBeingTransformed);
@@ -40,6 +37,12 @@ public class MCPatcherTransformer implements IClassTransformer {
             {
                 case 0:
                     transformInventoryComponent(classNode);
+                    break;
+                case 1:
+                    transformTileShelf(classNode);
+                    break;
+                case 2:
+                    transformTileShelfFiller(classNode);
                     break;
             }
 
@@ -50,6 +53,85 @@ public class MCPatcherTransformer implements IClassTransformer {
             e.printStackTrace();
         }
         return classBeingTransformed;
+    }
+
+    private void transformTileShelfFiller(ClassNode classNode) {
+        final String GET_MAIN_TILE = "getMainTile";
+        final String GET_MAIN_TILE_DESC = "()Lcom/cout970/magneticraft/tileentity/shelf/TileShelvingUnit;";
+        final String READ_NBT = ObfHelper.isObfuscated()?"func_145839_a":"readFromNBT";
+        final String READ_NBT_DESC = "(Lnet/minecraft/nbt/NBTTagCompound;)V";
+        final String WRITE_NBT = ObfHelper.isObfuscated()?"func_145841_b":"writeToNBT";
+        final String WRITE_NBT_DESC = "(Lnet/minecraft/nbt/NBTTagCompound;)V";
+        for (MethodNode method : classNode.methods) {
+            if (method.name.equals(GET_MAIN_TILE) && method.desc.equals(GET_MAIN_TILE_DESC)) {
+                System.out.println("Found/Patching TileShelfFiller:getMainTile");
+                LabelNode newLabelNode = new LabelNode();
+
+                InsnList toInsert = new InsnList();
+                toInsert.add(new VarInsnNode(ALOAD, 0));
+                toInsert.add(new FieldInsnNode(GETFIELD, "com/cout970/magneticraft/tileentity/shelf/TileShelfFiller", "offset", "Lcom/cout970/magneticraft/api/util/VecInt;"));
+                toInsert.add(new JumpInsnNode(IFNONNULL, newLabelNode));
+                toInsert.add(new InsnNode(ACONST_NULL));
+                toInsert.add(new InsnNode(ARETURN));
+                toInsert.add(newLabelNode);
+
+                method.instructions.insertBefore(method.instructions.getFirst(), toInsert);
+            } else if (method.name.equals(WRITE_NBT) && method.desc.equals(WRITE_NBT_DESC)) {
+                System.out.println("Found TileShelfFiller:writeToNBT");
+                AbstractInsnNode targetNode = ASMHelper.findFirstInstructionWithOpcode(method, INVOKESPECIAL);
+                if (targetNode != null) {
+                    System.out.println("Found TileShelfFiller:writeToNBT");
+                    LabelNode newLabelNode = new LabelNode();
+
+                    InsnList toInsert = new InsnList();
+                    toInsert.add(new VarInsnNode(ALOAD, 0));
+                    toInsert.add(new FieldInsnNode(GETFIELD, "com/cout970/magneticraft/tileentity/shelf/TileShelfFiller", "offset", "Lcom/cout970/magneticraft/api/util/VecInt;"));
+                    toInsert.add(new JumpInsnNode(IFNONNULL, newLabelNode));
+                    toInsert.add(new InsnNode(RETURN));
+                    toInsert.add(newLabelNode);
+                    
+                    method.instructions.insert(targetNode, toInsert);
+                }
+            } else if (method.name.equals(READ_NBT) && method.desc.equals(READ_NBT_DESC)) {
+                System.out.println("Found TileShelfFiller:readFromNBT");
+                AbstractInsnNode targetNode = ASMHelper.findFirstInstructionWithOpcode(method, INVOKESPECIAL);
+                if (targetNode != null) {
+                    System.out.println("Found TileShelfFiller:readFromNBT");
+                    LabelNode newLabelNode = new LabelNode();
+
+                    InsnList toInsert = new InsnList();
+                    toInsert.add(new VarInsnNode(ALOAD, 1));
+                    toInsert.add(new LdcInsnNode("offsetX"));
+                    toInsert.add(new MethodInsnNode(INVOKEVIRTUAL, "net/minecraft/nbt/NBTTagCompound", ObfHelper.isObfuscated()?"func_74764_b":"hasKey", "(Ljava/lang/String;)Z", false));
+                    toInsert.add(new JumpInsnNode(IFNE, newLabelNode));
+                    toInsert.add(new InsnNode(RETURN));
+                    toInsert.add(newLabelNode);
+
+                    method.instructions.insert(targetNode, toInsert);
+                }
+            }
+        }
+    }
+
+    private void transformTileShelf(ClassNode classNode) {
+        final String GET_INVENTORY = "getInventory";
+        final String GET_INVENTORY_DESC = "()Lcom/cout970/magneticraft/util/InventoryResizable;";
+        for (MethodNode method : classNode.methods) {
+            if (method.name.equals(GET_INVENTORY) && method.desc.equals(GET_INVENTORY_DESC)) {
+                System.out.println("Found/Patching TileShelf:getInventory");
+                LabelNode newLabelNode = new LabelNode();
+                
+                InsnList toInsert = new InsnList();
+                toInsert.add(new VarInsnNode(ALOAD, 0));
+                toInsert.add(new MethodInsnNode(INVOKEVIRTUAL, "com/cout970/magneticraft/tileentity/shelf/TileShelf", "getOffset", "()Lcom/cout970/magneticraft/api/util/VecInt;", false));
+                toInsert.add(new JumpInsnNode(IFNONNULL, newLabelNode));
+                toInsert.add(new InsnNode(ACONST_NULL));
+                toInsert.add(new InsnNode(ARETURN));
+                toInsert.add(newLabelNode);
+                
+                method.instructions.insertBefore(method.instructions.getFirst(), toInsert);
+            }
+        }        
     }
 
     private void transformInventoryComponent(ClassNode classNode) {
